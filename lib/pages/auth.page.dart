0759e77a97114f1ff9a720e06d29f35254a8965c
Filipe Braga <filipebragabrasil@hotmail.com>
2020-07-14 09:08:17 -0300

    Pré-organização de layout da página de login

--- lib/pages/auth.page.dart
+++ lib/pages/auth.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 
 class AuthPage extends StatelessWidget {
@@ -6,27 +7,38 @@ class AuthPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Center(
             child: Container(
               decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
-                borderRadius: BorderRadius.circular(5)
+                borderRadius: BorderRadius.circular(5),
               ),
               alignment: Alignment.center,
               width: MediaQuery.of(context).size.width - 40,
-              height: 220,
+              height: 300,
               child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Text("CONTA", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),),
-                    PersonalInput(),
-                    SizedBox(height: 20,),
-                    Text("SENHA", style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),),
-                    PersonalInput(),
+                    Row(
+                      children: <Widget>[
+                        PersonalInput(title: "000000", width: MediaQuery.of(context).size.width / 2.5, maxLength: 6),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 18),
+                          child: Text("-", style: TextStyle(color: Theme.of(context).primaryColor, fontWeight: FontWeight.bold, fontSize: 30),),
+                        ),
+                        PersonalInput(title: "00", width: MediaQuery.of(context).size.width / 3.5, maxLength: 2),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    PersonalInput(title: "••••••", isPassword: true),
+                    SizedBox(height: 20),
+                    RaisedButton(child: Text("Entrar"), onPressed: () {}),
                   ],
                 ),
               ),
@@ -39,26 +51,38 @@ class AuthPage extends StatelessWidget {
 }
 
 class PersonalInput extends StatelessWidget {
-
   bool isPassword;
+  String title;
+  double width;
+  int maxLength;
 
-  PersonalInput({this.isPassword = false});
+  PersonalInput({this.title = "", this.isPassword = false, this.width = 0, this.maxLength = 0});
 
   @override
   Widget build(BuildContext context) {
+
+    if (width == 0)
+      width = MediaQuery.of(context).size.width;
+
     return Container(
+      width: width,
       decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(8)
-      ),
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [BoxShadow(color: Colors.grey[300], blurRadius: 10)]),
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: TextFormField(
+          decoration: InputDecoration.collapsed(hintText: title),
+          keyboardType: TextInputType.number,
           obscureText: isPassword,
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Theme.of(context).primaryColor),
+          maxLength: maxLength == 0 ? null : maxLength,
+          style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Theme.of(context).primaryColor),
         ),
       ),
     );
   }
 }
-
